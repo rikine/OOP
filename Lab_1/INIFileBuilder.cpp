f3cd958577debb0fe4c6d5a3a5fa698bc28bff04
Niki <nikitos3046@yandex.ru>
2020-10-09 15:07:03 +0500

    Update INIFileBuilder.cpp

--- Lab_1/INIFileBuilder.cpp
+++ Lab_1/INIFileBuilder.cpp
@@ -3,6 +3,6 @@
 void INIFileBuilder::AddSection(const std::string &sectionName, const std::map<std::string, std::string> &keys)
 {
     if (data.find(sectionName) != data.end())
-        throw MyException("Same sections in one file : ", sectionName.c_str());
+        throw ParseError(sectionName.c_str());
     data.insert(make_pair(sectionName, keys));
 }
