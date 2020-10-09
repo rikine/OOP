1af47df5fc0f142049393df5dbbcc62fdacc99bb
Niki <nikitos3046@yandex.ru>
2020-10-09 15:09:04 +0500

    Update MyException.h

--- Lab_1/MyException.h
+++ Lab_1/MyException.h
@@ -2,20 +2,46 @@
 #include <exception>
 #include <string.h>
 
-class MyException : public std::exception
+class GetParamError : public std::exception
+{
+public:
+    GetParamError() = default;
+
+    const char *what() const noexcept override { return "No such section in this file "
+                                                        "or no such key in this section."; }
+};
+
+class InputFileError : public std::exception
+{
+public:
+    InputFileError() = default;
+
+    const char *what() const noexcept override { return "File isn't open. Example: ./INIParser "
+                                                        "/home/rikine/Documents/FILE.ini"; }
+};
+
+class ParseError : public std::exception
+{
+    char *msg_;
+
+public:
+    ParseError(const char *msg);
+
+    const char *what() const noexcept override { return "Invalid file. Parsing error with line: "; }
+
+    char *moreInfo() const noexcept { return msg_; }
+
+    ~ParseError() { delete[] msg_; }
+};
+
+/*class MyException : public std::exception
 {
     const char *msg_;
 
 public:
-    MyException(const char *msg1, const char *msg2 = "", const char *msg3 = "", const char *msg4 = "");
+    MyException(const char *msg) : msg_(msg) {}
 
     const char *what() const noexcept override { return msg_; }
 
     ~MyException() override { delete[] msg_; }
-};
-
-class GettingParamError : public MyException
-{
-public:
-    GettingParamError(const char *msg1, const char *msg2) : MyException(msg1, msg2) {}
-};
+};*/
