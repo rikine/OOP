c24f85e8a28b7fd916bb3d0e372a5d0377515e8d
Niki <nikitos3046@yandex.ru>
2020-10-09 15:08:40 +0500

    Update MyException.cpp

--- Lab_1/MyException.cpp
+++ Lab_1/MyException.cpp
@@ -1,11 +1,7 @@
 #include "MyException.h"
 
-MyException::MyException(const char *msg1, const char *msg2, const char *msg3, const char *msg4)
+ParseError::ParseError(const char *msg)
 {
-    char *msg = new char[200];
-    msg = strcat(msg, msg1);
-    msg = strcat(msg, msg2);
-    msg = strcat(msg, msg3);
-    msg = strcat(msg, msg4);
-    msg_ = msg;
+    msg_ = new char[150];
+    strcpy(msg_, msg);
 }
