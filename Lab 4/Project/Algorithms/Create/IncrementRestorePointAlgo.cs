563ec0fca8c616bd758c7344b7c678a94db921d5
Niki <nikitos3046@yandex.ru>
2020-11-27 19:53:22 +0500

    Update IncrementRestorePointAlgo.cs

--- Lab 4/Project/Algorithms/Create/IncrementRestorePointAlgo.cs
+++ Lab 4/Project/Algorithms/Create/IncrementRestorePointAlgo.cs
@@ -10,47 +10,22 @@ public class IncrementRestorePointAlgo : IAlgorithmCreate
             throw new ListIsEmptyException($"Empty list. Can't create Restore Point");
         if (restorePoints.Count == 0)
             throw new IncrementRestorePointException("No restore points. Can't create an increment restore point");
-        var lastFullRestore = restorePoints.Find(point => point.Incriment == false);
-        var lastIncrementRestore = restorePoints.Find(point => point.Incriment == true);
+        var lastFullRestore = restorePoints.FindLast(point => point.Incriment == false);
         if (lastFullRestore == null)
             throw new IncrementRestorePointException("No full restore point. Can't create an increment restore point");
 
         var filesForRestorePoint = new List<FileWithInfo>();
         var lstOfFilesFullRestorePoint = lastFullRestore.GetFiles();
-        if (lastIncrementRestore == null)
+        foreach (var file in files)
         {
-            foreach (var file in files)
-            {
-                var fileInFull = lstOfFilesFullRestorePoint.Find(file1 => file1.Path == file.Path);
-                if (fileInFull == null || fileInFull.Size != file.Size)
-                    filesForRestorePoint.Add(file);
-            }
-        }
-        else
-        {
-            var lstOfFilesIncRestorePoint = lastIncrementRestore.GetFiles();
-            foreach (var file in files)
-            {
-                var fileInFull = lstOfFilesFullRestorePoint.Find(file1 => file1.Path == file.Path);
-                var fileInInc = lstOfFilesIncRestorePoint.Find(file1 => file1.Path == file.Path);
-                if (fileInInc == null && fileInFull == null)
-                    filesForRestorePoint.Add(file);
-                else if (fileInInc != null)
-                {
-                    if (fileInInc.Size != file.Size)
-                        filesForRestorePoint.Add(file);
-                }
-                else if (fileInFull != null)
-                {
-                    if (fileInFull.Size != file.Size)
-                        filesForRestorePoint.Add(file);
-                }
-            }
+            var fileInFull = lstOfFilesFullRestorePoint.Find(file1 => file1.Path == file.Path);
+            if (fileInFull == null || fileInFull.Size != file.Size)
+                filesForRestorePoint.Add(file);
         }
 
         if (filesForRestorePoint.Count == 0)
             return null;
-            
+
         return new RestorePoint(filesForRestorePoint, filesForRestorePoint.Sum(file => file.Size), DateTime.Now, true);
     }
 }
